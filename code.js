568880f8c423a2e11bbf1b3fb77b60b6af832227
srinivas <srinivas.panuganti56@gmail.com>
2020-01-27 00:10:02 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,25 @@
+<style>
+  body * {
+    margin: 10px;
+    border: 1px solid blue;
+  }
+</style>
+
+<form>FORM
+  <div>DIV
+    <p>P</p>
+  </div>
+</form>
+
+<script>
+  for(let elem of document.querySelectorAll('*')) {
+    elem.addEventListener("click", e => alert(`Capturing: ${elem.tagName}`), true);
+    elem.addEventListener("click", e => alert(`Bubbling: ${elem.tagName}`));
+  }
+</script>
+
+
+/* to stop the propagation 
+<body onclick="alert(`the bubbling doesn't reach here`)">
+  <button onclick="event.stopPropagation()">Click me</button>
+</body>*/
